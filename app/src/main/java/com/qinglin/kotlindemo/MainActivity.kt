ac8fb04673f6161b0c9947fec9dad8c1568f4572
heqinglin <heqinglin@autohome.com.cn>
2017-07-11 19:18:50 +0800

    空指针处理测试

--- app/src/main/java/com/qinglin/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/qinglin/kotlindemo/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import java.io.StringReader
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val TAG = "hql"
@@ -16,41 +15,114 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         initView()
         test()
-
         test2()
+        testNull()
     }
 
-    private fun test2(){
-      Log.e(TAG,max(5,8).toString())
+    private fun testNull() {
+        printPrice("8", "24")
+        printPrice2("24","8")
+        //正确：自动检测变量类型
+        var list = listOf("何某某", null)
+        println("size:" + list.size)
+        //错误：检测到类型为空，不能引用到size
+//        var list = null
+//        println("size:" + list.size)
+
+        for (entity in list){
+            println(entity)
+            //判断类型后不用强转
+            if(entity is String){
+                println(entity.length)
+            }
+        }
     }
 
-    private fun max(a:Int,b:Int):Int{
+    private fun printPrice(arg1: String, arg2: String) {
+        var x = parseInt(arg1)
+        var y = parseInt(arg2)
+        //直接进行可能null的运算，编译不过去
+//        println(x*y)
+
+        //需要强制判断，才能编译过去，如下：（在编译期就避免空指针）
+        if (x != null && y != null) {
+            println(x * y)
+        }
+    }
+
+    private fun printPrice2(arg1: String, arg2: String) {
+        var x = parseInt(arg1)
+        var y = parseInt(arg2)
+        //直接进行可能null的运算，编译不过去
+//        println(x*y)
+        //只判断一个也不能编译过去
+//        if (x == null) {
+//            return
+//        }
+//        println(x * y)
+
+        //需要强制判断，才能编译过去，如下：（在编译期就避免空指针）
+        if (x == null) {
+            return
+        }
+        if (y == null) {
+            return
+        }
+        println(x * y)
+    }
+
+    fun parseInt(str: String): Int? {
+        try {
+            return str.toInt()
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    private fun test2() {
+        Log.e(TAG, max(5, 8).toString())
+
+        //创建一个对象，不用new
+        val g = Greeter("丁彦雨航")
+        g.greet()
+    }
+
+
+    class Greeter(val name: String) {
+        val TAG = "hql"
+        fun greet() {
+            println(TAG + ":Hello, $name")
+        }
+    }
+
+
+    private fun max(a: Int, b: Int): Int {
 //        if(a>b){
 //            return a
 //        }else{
 //            return b
 //        }
         //由于支持这种简便写法，所以，koltin没有三元二次表达式
-        return if(a>b) a else b
+        return if (a > b) a else b
     }
 
-    private  fun test(){
-        print(sum(2,3))
-        Log.e(TAG,"hql")
-        Log.e(TAG,sum(2,3).toString())
+    private fun test() {
+        println(TAG + ":" + sum(2, 3))
+        Log.e(TAG, "hql")
+        Log.e(TAG, sum(2, 3).toString())
 
 
         var a: Int = 1000
-        Log.e(TAG,(a === a).toString())
+        Log.e(TAG, (a === a).toString())
 
         //当我们需要变大一个可为null的引用时，就不能使用基本类型了，这种情况下装箱为数值对象
         //注：装箱不包吃对象的同一性，
-        val boxA :Int? = a  //可为null
-        val boxB :Int? = a  //可为null
+        val boxA: Int? = a  //可为null
+        val boxB: Int? = a  //可为null
 
-        Log.e(TAG,(boxA === boxB).toString())  //对象不相等
+        Log.e(TAG, (boxA === boxB).toString())  //对象不相等
 
-        Log.e(TAG,(boxA == boxB).toString())  //装箱会保持内容对象相等
+        Log.e(TAG, (boxA == boxB).toString())  //装箱会保持内容对象相等
 
         //以下为假想代码, 实际上是无法编译的:
 //        val c:Int? = 1 // 装箱为int类型
@@ -58,38 +130,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //        val d:Long? = c //Long
 //
 //        Log.e(TAG,(c == d).toString())  //false,因为 Long 的 equals() 方法会检查比较对象, 要求对方也是一个 Long 对象
-          //val定义的变量只读，不可修改
+        //val定义的变量只读，不可修改
 //        val e = 1
 //        e=e+1
 //        Log.e(TAG,(e+1).toString())
 
-         //var定义的变量时可变变量
+        //var定义的变量时可变变量
         var f = 2
 
-        f = f+1
+        f = f + 1
         f++
-        f+=2
-        Log.e(TAG,(f+1).toString())
+        f += 2
+        Log.e(TAG, (f + 1).toString())
 
         //字符串模版的运用
-        val arr1 = arrayOf("你妹","你大爷","就是我")
+        val arr1 = arrayOf("你妹", "你大爷", "就是我")
 
-        for (str in arr1){
-            Log.e(TAG,"我就是:${str}")
+        for (str in arr1) {
+            Log.e(TAG, "我就是:${str}")
         }
-        val name : String = "令狐冲"
-        tv_world!!.text = "我就是${name}"
     }
 
-    private fun sum(a :Int,b:Int):Int{
-        return a+b
+    private fun sum(a: Int, b: Int): Int {
+        return a + b
     }
 
     private fun initView() {
         tv_world = findViewById(R.id.tv_world) as TextView
-
         tv_world!!.setOnClickListener(this)
+        //字符模板的运用
+        val name: String = "令狐冲"
         val tv_world2 = findViewById(R.id.tv_world2) as TextView
+        tv_world2.text = "我就是${name}"
         tv_world2.setOnClickListener { tv_world!!.text = "tv_world2" }
     }
 
