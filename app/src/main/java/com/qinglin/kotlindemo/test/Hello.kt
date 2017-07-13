99a3274e67d868f84561fa69b1b40408a9ef6445
heqinglin <heqinglin@autohome.com.cn>
2017-07-13 18:54:12 +0800

    Lambda 表达式的用法

--- app/src/main/java/com/qinglin/kotlindemo/test/Hello.kt
+++ app/src/main/java/com/qinglin/kotlindemo/test/Hello.kt
@@ -12,16 +12,27 @@ fun main(args: Array<String>) {
 //        hello("何清林")
 //        fun sayhello(name:String) = hello(name)
 //        sayhello("李寻欢")
-    fun mParseInt(num:String) = parseInt(num)
+    fun mParseInt(num: String) = parseInt(num)
 //        将函数当作入参
-    lock(ReentrantLock(),{hello()})
+    lock(ReentrantLock(), { hello() })
+
+    //Lambda 表达式的用法
+    var ints = listOf(1, 2, 3, 4, 5, 6)
+    var intsSquare = ints.map { x -> x * x }
+    println(intsSquare)
+    //注意,如果一个函数字面值(function literal)只有唯一一个参数, 那么这个参数的声明可以省略(-> 也可以一起省略), 参数声明省略后, 将使用默认名称 it
+    var ints2 = ints.map { it * it }
+    println(ints2)
+    //过滤
+    var ints3 = ints.filter { it%2 == 0 }
+    println(ints3)
 }
 
 //    private fun hello(name: String) {
 //        println("Hello:" + name)
 //    }
 
-fun hello():String{
+fun hello(): String {
     println("Hello")
     return "hello"
 }
@@ -33,13 +44,12 @@ fun <T> lock(lock: Lock, body: () -> T): T {
     lock.lock()
     try {
         return body()
-    }
-    finally {
+    } finally {
         lock.unlock()
     }
 }
 
-fun parseInt(x:String):Int {
+fun parseInt(x: String): Int {
     return x.toInt()
 }
 
